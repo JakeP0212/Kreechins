416e1d959d49578953a32df4cf588e435b347ba0
Jake Puebla <jakepuebla001@gmail.com>
2026-01-15 03:09:42 -0800

    Starter stats + lvl. up moves for all Transformations

--- Game.java
+++ Game.java
@@ -7,9 +7,9 @@ class Game {
         ArrayList<BaseKreechin> kreechins = KreechinSetup.setup();
 
         Kreechin a = new Kreechin(kreechins.get(0), 10, false, new int[]{1, 2, 0, 0, 0}, 2);
-        Kreechin b = new Kreechin(kreechins.get(1), 10, true, new int[]{1, 3, 0, 0, 0}, 1);
-        Kreechin c = new Kreechin(kreechins.get(2), 10, true, new int[]{1, 4, 0, 0, 0}, 4);
-        Kreechin d = new Kreechin(kreechins.get(1), 10, false, new int[]{1, 3, 0, 0, 0}, 3);
+        Kreechin b = new Kreechin(kreechins.get(3), 10, true, new int[]{1, 3, 0, 0, 0}, 1);
+        Kreechin c = new Kreechin(kreechins.get(6), 10, true, new int[]{1, 4, 0, 0, 0}, 4);
+        Kreechin d = new Kreechin(kreechins.get(3), 10, false, new int[]{1, 3, 0, 0, 0}, 3);
 
         Team team1 = new Team(new Kreechin[]{a, b});
         Team team2 = new Team(new Kreechin[]{c, d});
